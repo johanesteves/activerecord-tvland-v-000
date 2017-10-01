33344bd19e08572835af1c00958ab231fe4cc10e
Johan Esteves <jsesteves91@gmail.com>
2017-10-01 23:56:36 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,7 +1,6 @@
-
 class CreateCharacters  < ActiveRecord::Migration
   def change
-    create_table :actors do |t| #we get a block variable here for the table
+    create_table :characters do |t| #we get a block variable here for the table
       t.string :first_name
       t.integer :show_id
     end
