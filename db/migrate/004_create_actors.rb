742e4201686beeef3a8f1bcead44a8002b4d346b
Johan Esteves <jsesteves91@gmail.com>
2017-10-01 23:55:37 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -3,16 +3,14 @@
 #by convention, the class name should match the part of the
 #file name after the number, so in this case:
 #001_create_networks.rb becomes class CreateNetworks
-class CreateNetworks < ActiveRecord::Migration
+class CreateActors  < ActiveRecord::Migration
   #define a change method in which to do the migration
   def change
-    create_table :networks do |t| #we get a block variable here for the table
-      #primary key of :id is created for us!
-      # defining columns is as simple as t.[datatype] :column
-      t.string :call_letters
+    create_table :actors do |t| #we get a block variable here for the table
+      t.string :first_name
       # the above breaks down to
       # "create a column called :call_letters on table t with type string
-      t.integer :channel
+      t.string :last_name
     end
   end
 end
