65900aa9e5d059bc00e8c6860671d5d9c20ff1e3
Johan Esteves <jsesteves91@gmail.com>
2017-10-01 23:59:06 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,10 +3,7 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def first_name
-    self.first_name
+    "#{self.first_name} #{self.last_name}"
   end
 
-  def last_name
-    self.last_name
-  end
 end
